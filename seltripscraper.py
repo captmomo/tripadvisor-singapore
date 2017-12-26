895a77bc57cb1f1d7a5445b4a939d8029bcc67dd
captmomo <wy4786@gmail.com>
2017-12-26 23:39:54 +0800

    Add files via upload

--- seltripscraper.py
+++ seltripscraper.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver import DesiredCapabilities
 import re
+import csv
+import sys
 
 def set_headers():
     desired_capabilities = DesiredCapabilities.PHANTOMJS.copy()
@@ -15,10 +17,11 @@ def set_headers():
 def scrape_with_headers(url):
     driver = set_headers()
     driver.get(url)
-    time.sleep(5)
-    element1 = driver.find_element_by_css_selector('span[class*="taLnk ulBlueLinks"]')
-    element1.click()
-    time.sleep(5)
+    time.sleep(2)
+    element1 = driver.find_elements_by_css_selector('span[class*="taLnk ulBlueLinks"]')
+    if len(element1) != 0:
+        element1[0].click()
+        time.sleep(2)
     html = (driver.page_source).encode('utf-8')
     extract_reviews(html, driver)
 
@@ -26,25 +29,27 @@ def extract_reviews(html, driver):
     soup = BeautifulSoup(html, 'lxml')
     for review in soup.find_all("div", {"class": "prw_rup prw_reviews_basic_review_hsx"}):
         review_text = review.find("p",{"class": "partial_entry"}).text
-        print(review_text)
-        playFile.write(review_text+'\n')
+        wr = csv.writer(csvfile, dialect='excel')
+        wr.writerow([review_text])
+        
     navigation = soup.find("div", {"class" : "unified pagination north_star "})
     if navigation.find("span", {"class" : "nav next disabled"}) == None:
         next_button = driver.find_element_by_xpath("//div[@class='ui_button primary ' and text()='Next']")
         next_button.click()
-        time.sleep(5)
-        element1 = driver.find_element_by_css_selector('span[class*="taLnk ulBlueLinks"]')
-        element1.click()
-        time.sleep(5)
+        time.sleep(2)
+        element1 = driver.find_elements_by_css_selector('span[class*="taLnk ulBlueLinks"]')
+        if len(element1) != 0:
+            element1[0].click()
+            time.sleep(2)
         html = (driver.page_source).encode('utf-8')
         extract_reviews(html, driver)
     else:
         print("last page")
         driver.quit()
-        playFile.close()
-
-playFile = open('zooreviews.txt', 'w', encoding='utf-8')
-url = 'https://www.tripadvisor.com.sg/Attraction_Review-g294265-d324542-Reviews-or9640-Singapore_Zoo-Singapore.html'
+        csvfile.close()
+sys.setrecursionlimit(1500)
+csvfile = open('zooreviews2.csv', 'w+',encoding='utf-8', newline='')
+url = 'https://www.tripadvisor.com.sg/Attraction_Review-g294265-d324542-Reviews-Singapore_Zoo-Singapore.html'
 scrape_with_headers(url)
 
 #<span class="taLnk ulBlueLinks" onclick="widgetEvCall('handlers.clickExpand',event,this);">More</span>
