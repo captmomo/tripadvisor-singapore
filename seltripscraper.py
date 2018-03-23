8ef538efaa02371e619106b66f8583eb109a14d3
captmomo <wy4786@gmail.com>
2018-03-23 17:09:32 +0800

    Update seltripscraper.py
    switching to headless chrome as phantomjs is not longer supported.

--- seltripscraper.py
+++ seltripscraper.py
@@ -7,11 +7,9 @@ import csv
 import sys
 
 def set_headers():
-    desired_capabilities = DesiredCapabilities.PHANTOMJS.copy()
-    desired_capabilities['phantomjs.page.customHeaders.User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' \
-                                                                  'AppleWebKit/537.36 (KHTML, like Gecko) ' \
-                                                                  'Chrome/39.0.2171.95 Safari/537.36'
-    driver = webdriver.PhantomJS(desired_capabilities=desired_capabilities)
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(chrome_options=options)
     return driver
 
 def scrape_with_headers(url):
